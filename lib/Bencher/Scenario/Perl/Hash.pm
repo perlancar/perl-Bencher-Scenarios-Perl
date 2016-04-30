99a29b8ed081f5219e3eae30a4007408e6986d39
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-30 09:10:12 +0700

    Updates

--- lib/Bencher/Scenario/Perl/Hash.pm
+++ lib/Bencher/Scenario/Perl/Hash.pm
@@ -10,15 +10,20 @@ use warnings;
 our $scenario = {
     precision => 0.001,
     participants => [
-        {name => 'access', code_template => 'no warnings "void"; state $hash = <hash>; for ("001"..<lastkey>) { $hash->{$_} }'},
-        {name => 'delete', code_template => 'my $hash = <hash>; for ("001"..<lastkey>) { delete $hash->{$_} }'},
-        {name => 'insert', code_template => 'my $hash = {}; for ("001"..<lastkey>) { $hash->{$_} = 0 }'},
+        {name => 'access', code_template => 'no warnings "void"; state $hash = <hash>; for (<firstkey>..<lastkey>) { $hash->{$_} }'},
+        {name => 'delete', code_template => 'my $hash = <hash>; for (<firstkey>..<lastkey>) { delete $hash->{$_} }'},
+        {name => 'insert', code_template => 'my $hash = {}; for (<firstkey>..<lastkey>) { $hash->{$_} = 0 }'},
     ],
     datasets => [
         {
             name => 'h100',
             summary => 'A 100-key ("001".."100") hash',
-            args => {lastkey => "100", hash=>{ map {$_=>0} "001".."100" }},
+            args => {firstkey => "001", lastkey => "100", hash=>{ map {$_=>0} "001".."100" }},
+        },
+        {
+            name => 'h1k',
+            summary => 'A 1k-key ("0001".."1000") hash',
+            args => {firstkey => "0001", lastkey => "1000", hash=>{ map {$_=>0} "0001".."1000" }},
         },
     ],
 };
