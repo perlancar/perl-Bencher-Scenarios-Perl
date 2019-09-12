4cd1a12e88f2d1bdb6273ea0f4588c1aa69b828b
perlancar (on pc-office) <perlancar@gmail.com>
2019-09-12 18:58:12 +0700

    Add datasets to Perl::Swap

--- lib/Bencher/Scenario/Perl/Swap.pm
+++ lib/Bencher/Scenario/Perl/Swap.pm
@@ -10,8 +10,15 @@ use warnings;
 our $scenario = {
     summary => 'Benchmark swapping two variables',
     participants => [
-        {code_template => 'my ($a, $b, $tmp); for (1..1000) { $tmp = $a; $a = $b; $b = $tmp }'},
-        {code_template => 'my ($a, $b); for (1..1000) { ($a, $b) = ($b, $a) }'},
+        {code_template => 'my $a = <a>; my $b = <b>; my $tmp; for (1..1001) { $tmp = $a; $a = $b; $b = $tmp } [$a, $b]'},
+        {code_template => 'my $a = <a>; my $b = <b>; for (1..1001) { ($a, $b) = ($b, $a) } [$a, $b]'},
+    ],
+    datasets => [
+        {name=>'undef', args=>{a=>undef, b=>undef}},
+        {name=>'empty-string', args=>{a=>'', b=>''}},
+        {name=>'short-string', args=>{a=>'12345', b=>'54321'}},
+        {name=>'long-string', args=>{a=>'1' x 100, b=>'2' x 100}},
+        {name=>'number', args=>{a=>1, b=>2}},
     ],
 };
 
